63861d0bc6d75a8c0c14a813654f3bd492b18b4c
KiSSel3 <andreyk042004@gmail.com>
2024-11-15 15:32:06 +0300

    Created angular project

---
+++ client/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import {AuthPageComponent} from './features/auth/pages/auth-page/auth-page.component';
+
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthPageComponent
+  }
+];
