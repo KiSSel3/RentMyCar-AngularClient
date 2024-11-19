7c39d5441c91e26a506653aabc3b1364f767bab2
KiSSel3 <andreyk042004@gmail.com>
2024-11-19 20:23:02 +0300

    Added user layout

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import {AuthPageComponent} from './features/auth/pages/auth-page/auth-page.component';
 import {CatalogPageComponent} from './features/rent-offer/pages/catalog-page/catalog-page.component';
+import {UserLayoutComponent} from './layouts/user-layout/user-layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: CatalogPageComponent
+    component: UserLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: CatalogPageComponent
+      }
+    ]
   },
   {
     path: 'auth',
