392469987accaabe820973520c84261d3942646c
KiSSel3 <andreyk042004@gmail.com>
2024-11-21 00:28:12 +0300

    Created profile page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import {AuthPageComponent} from './features/auth/pages/auth-page/auth-page.component';
 import {CatalogPageComponent} from './features/rent-offer/pages/catalog-page/catalog-page.component';
 import {UserLayoutComponent} from './layouts/user-layout/user-layout.component';
+import {ProfilePageComponent} from './features/user/pages/profile-page/profile-page.component';
+import {authGuard} from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -11,6 +13,11 @@ export const routes: Routes = [
       {
         path: '',
         component: CatalogPageComponent
+      },
+      {
+        path: 'profile',
+        component: ProfilePageComponent,
+        canActivate: [authGuard]
       }
     ]
   },
