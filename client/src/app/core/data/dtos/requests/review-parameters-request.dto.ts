d16a854836735f14487e9ca42691fbcfeabd43fd
KiSSel3 <andreyk042004@gmail.com>
2024-11-25 22:38:14 +0300

    Created base review service

---
+++ client/src/app/core/data/dtos/requests/review-parameters-request.dto.ts
@@ -0,0 +1,9 @@
+import {PaginationRequestDTO} from './pagination-request.dto';
+
+export interface ReviewParametersRequestDTO extends PaginationRequestDTO {
+  reviewerId?: string;
+  rentOfferId?: string;
+  minDate?: Date;
+  maxDate?: Date;
+  minRating?: number;
+}
---
+++ client/src/app/core/data/dtos/responses/review.dto.ts
@@ -0,0 +1,7 @@
+export interface ReviewDTO {
+  id: string;
+  reviewerId: string;
+  rentOfferId: string;
+  rating: number;
+  comment: string;
+}
